1fb83f4bae3b186060d6a24b6bdf9c4a459a2551
Mlie <emipa606@users.noreply.github.com>
2020-05-19 19:49:32 +0200

    Added correct biome and made them permanent enemy

---
+++ Source/DarkCreature/DarkCreature/Harmony.cs
@@ -0,0 +1,40 @@
+﻿using HarmonyLib;
+using RimWorld;
+using RimWorld.Planet;
+using Verse;
+
+namespace Horrors
+{
+
+    class HarmonyPatches
+    {
+        [StaticConstructorOnStartup]
+        internal static class HarmonyStart
+        {
+            static HarmonyStart()
+            {
+                new Harmony("mlie.horrors").PatchAll();
+            }
+        }
+
+        [HarmonyPatch(typeof(FactionGenerator), "GenerateFactionsIntoWorld")]
+        class Patch
+        {
+            static void Postfix()
+            {
+                var settlements = Find.WorldObjects.SettlementBases;
+                foreach (Settlement settlement in settlements)
+                {
+                    Log.Message(settlement.Faction.def.defName + " owns tile " + settlement.Tile + " with biome " + Find.WorldGrid[settlement.Tile].biome.defName);
+                    if (settlement.Faction.def.defName != "Horrors")
+                    {
+                        continue;
+                    }
+                    var settlementTile = Find.WorldGrid[settlement.Tile];
+                    settlementTile.biome = BiomeDef.Named("HorrorWastes");
+                }
+            }
+        }
+
+    }
+}
